459208379c6cf05bf29e50d8470fd16ad721623d
Michael Hayes <michael.hayes@mystenlabs.com>
2024-02-08 18:16:25 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
 import {
   ConnectButton,
-  useSuiClientQuery,
   useSignAndExecuteTransactionBlock,
   useCurrentAccount,
-  // useSuiClient,
 } from "@mysten/dapp-kit";
-import { Box, Container, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Container, Flex, Heading } from "@radix-ui/themes";
 import { ZkSendLinkBuilder } from "@mysten/zksend";
 import { useMutation } from "@tanstack/react-query";
 import { FUD_TYPE, ZK_SEND_HOST } from "./constants";
+import { useState } from "react";
+import {
+  MIST_PER_SUI,
+  SUI_TYPE_ARG,
+  isValidSuiAddress,
+  normalizeSuiAddress,
+} from "@mysten/sui.js/utils";
 
 function App() {
   const address = useCurrentAccount()?.address;
   const signAndExecute = useSignAndExecuteTransactionBlock();
-  // const client = useSuiClient();
+  const [coinType, setCoinType] = useState(FUD_TYPE);
+  const [balances, setBalances] = useState<Record<string, bigint>>({});
+  const [nfts, setNfts] = useState<string[]>([]);
 
   const createLink = useMutation({
-    mutationFn: async (amount: bigint) => {
+    mutationFn: async () => {
       const link = new ZkSendLinkBuilder({
         sender: address!,
         host: ZK_SEND_HOST,
         redirect: {
           url: window.location.href,
-          name: 'zkSend Wallet Demo',
-        }
+          name: "zkSend Wallet Demo",
+        },
       });
 
-      link.addClaimableBalance(FUD_TYPE, amount);
+      for (const [type, amount] of Object.entries(balances)) {
+        link.addClaimableBalance(type, amount);
+      }
+
+      for (const nft of nfts) {
+        link.addClaimableObject(nft);
+      }
 
       const txb = await link.createSendTransaction();
 
@@ -40,17 +53,6 @@ function App() {
       window.location.assign(url);
     },
   });
-
-  const balance = useSuiClientQuery(
-    "getBalance",
-    {
-      owner: address!,
-      coinType: FUD_TYPE,
-    },
-    {
-      enabled: !!address,
-    },
-  );
 
   return (
     <>
@@ -84,36 +86,131 @@ function App() {
             </Box>
           )}
 
-          <Box>address: {address}</Box>
-          <Box>balance: {balance.data?.totalBalance}</Box>
-
-          <form>
+          <Box>
+            <Heading>Add Sui</Heading>
             <label>
               Amount:
-              <input
-                id="fud-amount"
-                type="number"
-                step={1}
-                min={0}
-                defaultValue={100}
-                max={balance.data?.totalBalance}
-              />
+              <input id="sui-amount" type="number" defaultValue={0} />
             </label>
             <button
-              type="submit"
               onClick={async (ev) => {
                 ev.preventDefault();
-                createLink.mutate(
-                  BigInt(
-                    (document.getElementById("fud-amount") as HTMLInputElement)
-                      .value,
-                  ),
-                );
+                setBalances({
+                  ...balances,
+                  [SUI_TYPE_ARG]:
+                    (balances[SUI_TYPE_ARG] ?? 0n) +
+                    BigInt(
+                      Number(
+                        (
+                          document.getElementById(
+                            "sui-amount",
+                          ) as HTMLInputElement
+                        ).value,
+                      ) * Number(MIST_PER_SUI),
+                    ),
+                });
               }}
             >
-              Create link
+              Add
             </button>
-          </form>
+          </Box>
+          <Box>
+            <Heading>Add Coin</Heading>
+            <Box>
+              <label>
+                Coin Type:
+                <input
+                  id="coin-type"
+                  size={80}
+                  type="string"
+                  value={coinType}
+                  onChange={(ev) => setCoinType(ev.target.value)}
+                />
+              </label>
+            </Box>
+            <Box>
+              <label>
+                Amount:
+                <input id="coin-amount" type="number" defaultValue={0} />
+              </label>
+              <button
+                onClick={async (ev) => {
+                  ev.preventDefault();
+                  setBalances({
+                    ...balances,
+                    [coinType]:
+                      (balances[coinType] ?? 0n) +
+                      BigInt(
+                        (
+                          document.getElementById(
+                            "coin-amount",
+                          ) as HTMLInputElement
+                        ).value,
+                      ),
+                  });
+                }}
+              >
+                Add
+              </button>
+            </Box>
+          </Box>
+
+          <Box>
+            <Heading>Add Object</Heading>
+            <Box>
+              <label>
+                Object ID:
+                <input id="object-id" size={80} type="string" />
+              </label>
+              <button
+                onClick={async (ev) => {
+                  const el = document.getElementById(
+                    "object-id",
+                  ) as HTMLInputElement;
+                  const address = normalizeSuiAddress(el.value);
+                  ev.preventDefault();
+
+                  if (isValidSuiAddress(address)) {
+                    setNfts([...nfts, address]);
+                  }
+                  el.value = "";
+                }}
+              >
+                Add
+              </button>
+            </Box>
+          </Box>
+
+          <Box>
+            <Heading>Summary</Heading>
+            <Box>
+              <Heading>Balances</Heading>
+              <ul>
+                {Object.entries(balances).map(([type, amount]) => (
+                  <li key={type}>
+                    {type === SUI_TYPE_ARG
+                      ? Number(amount) / Number(MIST_PER_SUI)
+                      : String(amount)}{" "}
+                    {type}
+                  </li>
+                ))}
+              </ul>
+            </Box>
+            <Box>
+              <Heading>NFTs</Heading>
+              <ul>
+                {nfts.map((nft) => (
+                  <li key={nft}>{nft}</li>
+                ))}
+              </ul>
+            </Box>
+          </Box>
+          <Button
+            disabled={nfts.length === 0 && Object.keys(balances).length === 0}
+            onClick={() => createLink.mutateAsync()}
+          >
+            Create Link
+          </Button>
         </Container>
       </Container>
     </>
